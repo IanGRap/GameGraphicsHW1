0d217b90eedf762f49fc55dff27a6e8bf53bb129
Ian Rapoport <irapopor@ucsc.edu>
2019-04-15 16:37:47 -0700

    Added rotation and solid color materials

---
+++ Assets/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float xRotation;
+    public float yRotation;
+    public float zRotation;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(xRotation * Time.deltaTime, yRotation * Time.deltaTime, zRotation * Time.deltaTime);
+    }
+}
